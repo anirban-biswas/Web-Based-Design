d12ad5c95c4f47605a005a4775dac675d51c061a
anirban-biswas <36059843+anirban-biswas@users.noreply.github.com>
2018-02-26 18:27:25 -0600

    Html to PHP
    Changed Htlm pages to php to collaborate with database

---
+++ Login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>Login Form</title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+    <section>
+    <div class="loginbox">
+        <div class = "left">
+            <h1>Study Group</h1>
+            <p>This is a one of a kind platform providing 
+            students an opportunity to share their knowledge with their peers. Study Group helps you find peers in your university to do group study. It matches your peers with your need and their need so you can help eachother succeed in your classes.</p>
+        </div>
+        <div class = "right" >
+        <img src="avatar.png" class="avatar" />
+            <div class="formbox">
+        <h2>Login Here</h2>
+        <form>
+            <h4>Username</h4>
+            <input type="text" name="" placeholder="  Enter Username" />
+            <h4>Password</h4>
+            <input type="password" name="" placeholder="  Enter Password" />
+            <input type="submit" name="" value="Login" />
+            <a href="#">Forgot your Password?</a><br />
+            <a href="#">Forgot your Username?</a>
+            <p>Not a member yet? <a href="Registration.php"> Register here!</a></p>
+        </form>
+                </div>
+            </div>
+    </div>
+        </section>
+</body>
+</html>
---
+++ Registration.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>Sign Up Form</title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+    <section>
+    <div class="SignUpbox">
+        <div class = "left">
+            <div class="formbox">
+            <h1>Registration Form</h1>
+        <form>
+            <h4>First Name<br>
+            <input type="text" name="" placeholder="  Enter first name" />
+            <br>Last Name<br>
+            <input type="text" name="" placeholder="  Enter last name" />
+            <br>University/College<br>
+            <input type="text" name="" placeholder="  Enter University/College name" />
+            <br>Email Address<br>
+            <input type="text" name="" placeholder="  Enter your email" />
+            <br>Contact Number<br>
+            <input type="text" name="" placeholder="  Enter Contact Number" /></h4>
+        </form>
+            </div>
+        </div>
+        <div class = "right" >
+            <div class="formbox">
+        
+        <form>
+            <h4>Create Username<br>
+            <input type="text" name="" placeholder="  Enter Username" />
+            <br>Password<br>
+            <input type="password" name="" placeholder="  Enter Password" />
+            <br>Re-Enter Password<br>
+            <input type="password" name="" placeholder="  ReEnter Password" /></h4>
+            <input type="submit" name="" value="Sign Up" />
+            <p>Already a member? <a href="Login.php"> Register here!</a></p>
+        </form>
+                </div>
+            </div>
+    </div>
+        </section>
+</body>
+</html>
