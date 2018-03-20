07dae6ffaf3a148ef14dff39eb311084fdfad3f0
shreyathumma <shreyathumma@yahoo.com>
2018-03-19 20:45:14 -0500

    Main page

---
+++ main.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html> 
+<html>
+
+<head> 
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href= "style.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Amatic+SC" rel="stylesheet">
+
+	<title>STUDY GROUP</title>
+</head>
+
+<body> 
+  <h3 class=" title titlebg">Study Group</h3>
+    
+
+<div  class="icon-bar" >
+      <a class="active" href="main.html"><i class="fa fa-home fa-2x"></i>Home</a>
+      <a href="profile.html"><i class="fa fa-user fa-2x"></i>Profile</a> 
+      <a href="inbox.html"><i class="fa fa-comments fa-2x"></i>Inbox</a> 
+      <a href="groups.html"><i class="fa fa-users fa-2x"></i>Groups</a>
+      <a href="people.html"><i class="fa fa-user-plus fa-2x"></i>People</a>
+      <a href="settings.html"><i class="fa fa-cog fa-2x"></i>Settings</a> 
+</div>
+
+
+</body>
+</html>
