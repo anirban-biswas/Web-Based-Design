06251e80f86fdfa708e8bdb77a906e01601a8f58
Travis Maupin <maupintcm@gmail.com>
2018-03-19 19:33:06 -0500

    added Login
    added php for login and a version 1 logout

---
+++ core.php
@@ -0,0 +1,26 @@
+<?php
+ob_start();
+session_start();
+$current_file = $_SERVER['SCRIPT_NAME'];
+$http_referer = $_SERVER['HTTP_REFERER'];
+
+function loggedin() {
+	if (isset($_SESSION['user_name']) && !empty($_SESSION['user_name']))
+	{
+		return true;
+	}
+	else { return false; }
+}
+
+function getuserfield($conn, $field)
+{
+	$query = "SELECT `$field` FROM `user` WHERE `username` = '$_SESSION[user_name]'";
+	if ($query_run = mysqli_query($conn, $query))
+	{
+		if ( $query_result = mysqli_fetch_assoc($query_run))
+		{
+			return $query_result[$field];
+		}
+	}
+}
+?>
