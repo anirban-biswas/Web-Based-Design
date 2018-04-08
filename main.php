a877fe6465cace707d6615df4f673276621b0559
shreyathumma <shreyathumma@yahoo.com>
2018-04-07 20:57:02 -0500

    Update main.php

--- main.php
+++ main.php
@@ -4,7 +4,7 @@
 <head> 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="C:/Users/shreya thumma/Desktop/STUDY GROUP/css/style.css" />
+    <link rel="stylesheet" type="text/css" href="style.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css?family=Amatic+SC" rel="stylesheet">
 
