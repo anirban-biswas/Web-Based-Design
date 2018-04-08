1e593b522e4fab7b25b0bf7bc0b2b33716a5fdf6
shreyathumma <shreyathumma@yahoo.com>
2018-04-07 20:48:56 -0500

    Update main.php

--- main.php
+++ main.php
@@ -4,7 +4,7 @@
 <head> 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href= "style.css" />
+    <link rel="stylesheet" type="text/css" href="C:/Users/shreya thumma/Desktop/STUDY GROUP/css/style.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css?family=Amatic+SC" rel="stylesheet">
 
@@ -12,35 +12,55 @@
 </head>
 
 <body> 
+
 <div  class="icon-bar" >
       <a class="active" href="main.html"><i class="fa fa-home fa-2x"></i>Home</a>
       <a href="profile.html"><i class="fa fa-user fa-2x"></i>Profile</a> 
       <a href="inbox.html"><i class="fa fa-comments fa-2x"></i>Inbox</a> 
       <a href="groups.html"><i class="fa fa-users fa-2x"></i>Groups</a>
       <a href="people.html"><i class="fa fa-user-plus fa-2x"></i>People</a>
-      <a href="settings.html"><i class="fa fa-cog fa-2x"></i>Settings</a>
-      <a href="logout.php"><i class="fa fa-sign-out fa-2x"></i>Log out</a> 
+      <a href="logout.php"><i class="fa fa-sign-out fa-2x"></i>Sign out</a> 
 </div>
-	
-	<a href="main.php"><h1 class=" title titlebg">Study Group</h1></a>
-	
-<div class="dropdown">
+
+<div>
+  <a href="main.html"><h1 class= " position title titlebg">Study Group</h1></a>
+</div>
+  
+
+  <div class="dropdown">
   <button onclick="dropDown()" class="dropbtn">Select a topic</button>
   <div id="dropDown" class="dropdown-content">
-    <a href="#">Topic 1</a>
-    <a href="#">Topic 2</a>
-    <a href="#">Topic 3</a>
+    <button onclick="btnResult()">Topic 1</button>
+    <button onclick="btnResult()">Topic 2</button>
+    <button onclick="btnResult()">Topic 3</button>
+    <button onclick="btnResult()">Topic 4</button>
   </div>
+</div>
+
+<div class="resultbox" id="result">
+        <img src="avatar.png"/>
+        <a href="profile.php"><h3>Name</h3></a>
+        <p>College</p>
+        <p>Major</p>
+        <div class="strengths">
+          <h4>Strengths</h4>
+          <p>Topic 1</p>
+          <p>Topic 2</p>
+        </div>
+        <div class="weaknesses">
+          <h4>Weaknesses</h4>
+          <p>Topic 1</p>
+          <p>Topic 2</p>
+        </div>
+        
 </div>
 
 <script>
 function dropDown() {
     document.getElementById("dropDown").classList.toggle("show");
 }
-
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
-
     var dropdown = document.getElementsByClassName("dropdown-content");
     var i;
     for (i = 0; i < dropdown.length; i++) {
@@ -51,6 +71,11 @@ window.onclick = function(event) {
     }
   }
 }
+
+function btnResult() {
+  document.getElementById("result").style.visibility = "visible";
+}
+
 </script>
 
 </body>
