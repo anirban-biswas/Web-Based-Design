cc1e8a770c5883368ae91023f21c78617f2e74a6
shreyathumma <shreyathumma@yahoo.com>
2018-04-15 20:48:40 -0500

    Update Profile.php

--- Profile.php
+++ Profile.php
@@ -1,50 +1,77 @@
-    <!DOCTYPE html>
+<!DOCTYPE html>
 
-    <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-        <meta charset="utf-8">
+<html>
+<head>
+	<title>PROFILE</title>
+
+  <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href= "style.css" />
+    <link rel="stylesheet" type="text/css" href="style.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css?family=Amatic+SC" rel="stylesheet">
-    </head>
-    <body>
-        
+
+</head>
+<body>
+    
     <div  class="icon-bar" >
       <a href="main.php"><i class="fa fa-home fa-2x"></i>Home</a>
-      <a class="active" href="profile.php"><i class="fa fa-user fa-2x"></i>Profile</a> 
+      <a class="active" href="profile.html"><i class="fa fa-user fa-2x"></i>Profile</a> 
       <a href="inbox.php"><i class="fa fa-comments fa-2x"></i>Inbox</a> 
       <a href="groups.php"><i class="fa fa-users fa-2x"></i>Groups</a>
       <a href="people.php"><i class="fa fa-user-plus fa-2x"></i>People</a>
-      <a href="logout.php"><i class="fa fa-sign-out fa-2x"></i>Log out</a> 
-    </div>
-        
-    <a href="main.php"><h1 class="title titlebg position">Study Group</h1></a>
+      <a href="logout.php"><i class="fa fa-sign-out fa-2x"></i>Sign out</a> 
+   </div>
+
+   <a href="main.html"><h1 class="title titlebg position">Study Group</h1></a>
     
-    <div class="profile">
-      <img src="avatar.png"/>
-      <h3>Name</h3>
+    
+      <img class="profileImage" src="avatar.png"/>
+      <div class="ProfileBox">
+      <p>Name</p>
       <p>College</p>
       <p>Major</p>
-      <hr>
-      <button>Edit profile</button>
-    </div>
+      </div>
+
+      <a href="editprofile.html"><button class="profileButton">Edit Profile</button></a>
+      
 
     <div class="skills">
-      <h3>Skills</h3>
-      <h4>Strengths</h4>
-         <div class="boxed">
-            <p>Topic 1</p>
-            <p>Topic 2</p>
-            <p>Topic 3</p>
-         </div>
-      <h4>Weaknesses</h4>
-         <div class="boxed">
-            <p>Topic 1</p>
-            <p>Topic 2</p>
-         </div>
-         <button>Edit Skills</button>
+    <a href="editskills.html"><button>Edit Skills</button></a>
+    
+      <table>
+          <tr>
+            <th>Subject</th>
+            <th>Strengths</th>
+            <th>Weaknesses</th>
+          </tr>
+          <tr>
+            <td>Subject 1</td>
+            <td>Strength 1, Strength2...</td>
+            <td>Weakness 1, Weaknss2...</td>
+          </tr>
+          <tr>
+            <td>Subject 2</td>
+            <td>Strength 1, Strength2...</td>
+            <td>Weakness 1, Weaknss2...</td>
+          </tr>
+          <tr>
+            <td>Subject 3</td>
+            <td>Strength 1, Strength2...</td>
+            <td>Weakness 1, Weaknss2...</td>
+          </tr>
+          <tr>
+            <td>Subject 4</td>
+            <td>Strength 1, Strength2...</td>
+            <td>Weakness 1, Weaknss2...</td>
+          </tr>
+          <tr>
+            <td>Subject 5</td>
+            <td>Strength 1, Strength2...</td>
+            <td>Weakness 1, Weaknss2...</td>
+          </tr>
+        </table>
     </div>
-        
-    </body>
-    </html>
+    
+
+</body>
+</html>
